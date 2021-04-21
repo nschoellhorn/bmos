b8f2f6a4ab88164efc2e30a0f6633804994805fe
Niklas Schöllhorn <schoellhorn.niklas@gmail.com>
2021-04-21 20:44:54 +0200

    Rewrite console font rendering
    In the code taken from the example of the old bootloader (bootboot), we
    were linking against a binary object file of the font and using this to
    render the characters. The new implementation simplifies a lot of the
    old code by just using a library to parse the embedded raw version of
    the font, which is included by the include_bytes!() macro.

--- src/console.rs
+++ src/console.rs
@@ -1,10 +1,7 @@
-use crate::graphics::{psf2_t, CursorPosition, Framebuffer, GraphicsSettings};
+use crate::graphics::{CursorPosition, Framebuffer, GraphicsSettings};
 use core::fmt::Write;
+use psf::Font;
 use spin::Mutex;
-
-extern "C" {
-    pub static mut _binary_font_psf_start: u64;
-}
 
 /*#[macro_export]
 macro_rules! print {
@@ -23,7 +20,7 @@ pub fn _print(args: core::fmt::Arguments) {
 }*/
 
 pub struct Console<'a> {
-    font: *mut psf2_t,
+    font: &'a Font<'a>,
     cursor: CursorPosition,
     graphics: &'a GraphicsSettings,
     framebuffer: &'a Mutex<Framebuffer>,
@@ -43,12 +40,12 @@ impl<'a> Console<'a> {
     pub fn init(
         gfx_settings: &'a GraphicsSettings,
         framebuffer: &'a Mutex<Framebuffer>,
-        font: *mut psf2_t,
+        font: &'a Font<'a>,
     ) -> Self {
         unsafe {
             Self {
-                char_width: (gfx_settings.width / ((*font).width + 1)),
-                char_height: (gfx_settings.height / ((*font).height + 1)),
+                char_width: (gfx_settings.width / (font.width() as u32 + 1)),
+                char_height: (gfx_settings.height / (font.height() as u32 + 1)),
                 graphics: gfx_settings,
                 font,
                 framebuffer,
@@ -76,7 +73,6 @@ impl<'a> Console<'a> {
             return;
         }
         unsafe {
-            let font = self.font as *mut psf2_t;
             let (mut line, mut mask, mut offs): (u64, u64, u32);
             let kx = if self.cursor.column >= self.char_width {
                 self.cursor.column = 0;
@@ -98,31 +94,26 @@ impl<'a> Console<'a> {
             } else {
                 self.cursor.row
             };
-            let bpl = ((*font).width + 7) / 8;
 
-            let glyph_a: *mut u8 = (font as u64 + (*font).headersize as u64) as *mut u8;
-            let mut glyph: *mut u8 = glyph_a.offset(
-                (if s as u8 > 0 && (s as u32) < (*font).numglyph {
-                    s as u32
-                } else {
-                    0
-                } * ((*font).bytesperglyph)) as isize,
-            );
-            for y in 0..(*font).height {
-                mask = 1 << ((*font).width - 1);
-                for x in 0..(*font).width {
+            let glyph = match self.font.get_char(s) {
+                Some(glyph) => glyph,
+                None => return,
+            };
+
+            for y in 0..self.font.height() as u32 {
+                for x in 0..self.font.width() as u32 {
                     let mut target_value: u32 = 0;
-                    if (*glyph as u64) & (mask) > 0 {
-                        target_value = color;
+                    if let Some(is_set) = glyph.get(x as usize, y as usize) {
+                        if is_set {
+                            target_value = color;
+                        }
                     }
                     framebuffer.draw_pixel(
-                        ((*font).width * kx) + x,
-                        ((*font).height * ky) + y,
+                        (self.font.width() as u32 * kx) + x,
+                        (self.font.height() as u32 * ky) + y,
                         target_value,
                     );
-                    mask >>= 1;
                 }
-                glyph = glyph.offset(bpl as isize);
             }
         }
         self.cursor.column += 1;
--- src/main.rs
+++ src/main.rs
@@ -15,26 +15,28 @@ mod graphics;
 mod serial;
 #[macro_use]
 mod vga_buffer;
+mod gdt;
 
 use crate::graphics::psf2_t;
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
         idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
 
+const FONT: &'static [u8] = include_bytes!("../font.psf");
+
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+    gdt::init();
     debug!("test");
     IDT.load();
-    debug!(
-        "interrupts: {}",
-        x86_64::instructions::interrupts::are_enabled()
-    );
+
     // First, set up basic graphics and a console to make sure we can print debug stuff
     if let bootloader::boot_info::Optional::None = boot_info.framebuffer {
         panic!("No framebuffer found! This is a problem.");
@@ -46,17 +48,42 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
         height: boot_fb.info().vertical_resolution as u32,
     };
 
+    let font = match psf::Font::parse_font_data(FONT) {
+        Err(error) => panic!("Failed to parse font data: {:?}", error),
+        Ok(font) => {
+            debug!("Parsed PSF font: {:?}", &font);
+            font
+        }
+    };
+
+    {
+        let mut framebuffer = framebuffer.lock();
+        framebuffer.clear();
+        framebuffer.draw_rect(100, 100, 100, 100, 0x6441A4);
+    }
+
+    let mut console = console::Console::init(&graphics_settings, &framebuffer, &font);
+    console.println("Hello");
+    console.println("World");
+
     loop {
         //println!("Tick");
         unsafe { asm!("hlt") };
     }
 }
 
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
 extern "x86-interrupt" fn double_fault_handler(
     stack_frame: InterruptStackFrame,
-    _error_code: u64,
+    error_code: u64,
 ) -> ! {
-    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+    panic!(
+        "EXCEPTION: DOUBLE FAULT, Error Code: {}\n{:#?}",
+        error_code, stack_frame
+    );
 }
 
 #[panic_handler]
