d24cdfa35de700bda56592ad635f19c8daf5e2e4
Niklas Schöllhorn <schoellhorn.niklas@gmail.com>
2021-04-22 21:35:59 +0200

    Add run script to make the dev process faster

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cargo kbuild
+if [ $? -ne 0 ]; then
+    echo "Failed to build kernel, check errors above."
+    exit
+fi
+cargo run --package=boot
+if [ $? -ne 0 ]; then
+    echo "Failed to build bootable image, check errors above."
+    exit
+fi
+qemu-system-x86_64 -drive format=raw,file=target/x86_64-bmos/debug/boot-bios-bmos.img  -d cpu_reset -serial stdio
